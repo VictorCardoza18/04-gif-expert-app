2cada13470320883f1372a5624b828809bee5f4f
Josue <vjosuelc@gmail.com>
2020-10-22 07:44:44 -0500

    p

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const GifExpertApp = (props) => {
+const GifExpertApp = () => {
+  // const categorias = ["One Punch", "Samurai X", "Dragon Ball"];
+  const [categorias, setCategorias] = useState([
+    "One Punch",
+    "Samurai X",
+    "Dragon Ball",
+  ]);
+
+  const handleAdd = () => {
+    setCategorias(["Kakegurui", ...categorias]);
+  };
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+
+      <button onClick={handleAdd}>Agregar</button>
+
+      <ol>
+        {categorias.map((categoria, indice) => (
+          <li key={categoria}> {categoria} </li>
+        ))}
+      </ol>
     </>
   );
 };
