7fa9c9c72c932e86867ac3d8a2a044a9c1f88435
Victor Cardoza <vjosuelc@gmail.com>
2020-11-04 21:53:58 -0600

    camviob

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -10,14 +10,15 @@
  * and re-run your build process.
  * See https://goo.gl/2aRDsh
  */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+  "/precache-manifest.d0822e85b70b74c5ef7dc881cae5b174.js"
 );
 
-importScripts("/precache-manifest.d0822e85b70b74c5ef7dc881cae5b174.js");
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
@@ -32,9 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL("/index.html"),
-  {
-    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
-  }
-);
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
