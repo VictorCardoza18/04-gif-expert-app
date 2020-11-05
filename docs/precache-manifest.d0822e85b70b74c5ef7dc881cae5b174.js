592b29ded0004d6c8c1ff3650e660cc62987a7f2
Victor Cardoza <vjosuelc@gmail.com>
2020-11-04 21:21:41 -0600

    Docs agregado

---
+++ docs/precache-manifest.d0822e85b70b74c5ef7dc881cae5b174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d871f8568144e9485fc743543ea3e960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6c34b2ca6b23aa5ad7f",
+    "url": "/static/css/main.a96d04de.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c34b2ca6b23aa5ad7f",
+    "url": "/static/js/main.e63422a4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
